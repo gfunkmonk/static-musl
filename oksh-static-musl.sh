8eb82a0267f6d53e9018da02d896325da0324cfd
Matt Rinsch <gfunkmonk@mailbox.org>
2026-03-24 18:31:35 -0400

    Optimize apk package installation in script
    Combine multiple apk add commands into one line for efficiency.

--- oksh-static-musl.sh
+++ oksh-static-musl.sh
@@ -23,15 +23,7 @@ run_build_setup "oksh" "${OKSH_VERSION}" "${OKSH_TARBALL}" \
 
 sudo chroot "./${CHROOTDIR}/" /bin/sh -s <<EOF
 set -e
-apk update
-apk add build-base \
-musl-dev \
-ccache \
-pkgconfig \
-ncurses-dev \
-ncurses-static \
-autoconf \
-patch
+apk update && apk add build-base musl-dev ccache pkgconfig ncurses-dev ncurses-static
 mkdir -p /ccache && export CCACHE_DIR=${CCACHE_CHROOT_DIR} CCACHE_BASEDIR=/ PATH=/usr/lib/ccache/bin:\$PATH
 chmod 755 upx
 tar xf oksh-${OKSH_VERSION}.tar.gz
