98636fed6277e77db83a8c7ba54f193db5f41ee0
Matt Rinsch <gfunkmonk@mailbox.org>
2026-03-27 02:28:13 -0400

    socat

---
+++ socat-static-musl.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -euo pipefail
+. "$(dirname "$0")/common.sh"
+
+SOCAT_VERSION="1.8.1.1"
+PACKAGE_VERSION="${SOCAT_VERSION}"
+SOCAT_TARBALL="socat-${SOCAT_VERSION}.tar.gz"
+SOCAT_MIRRORS=(
+  "http://www.dest-unreach.org/socat/download/socat-1.8.1.1.tar.gz"
+  "https://fossies.org/linux/privat/socat-1.8.1.1.tar.gz"
+  "https://distfiles.alpinelinux.org/distfiles/edge/socat-1.8.1.1.tar.gz"
+)
+
+run_build_setup "socat" "${SOCAT_VERSION}" "${SOCAT_TARBALL}" \
+  "hotfix-const-correctness.patch" \
+  -- "${SOCAT_MIRRORS[@]}"
+
+sudo chroot "./${CHROOTDIR}/" /bin/sh -s <<EOF
+set -e
+echo -e "${ORANGE}= Installing dependencies...${NC}"
+apk update && apk add build-base mold ccache pkgconfig openssl-dev openssl-libs-static readline-dev readline-static ncurses-dev \
+  ncurses-static ncurses-terminfo-base linux-headers tar
+apk upgrade musl-dev mold --repository=https://dl-cdn.alpinelinux.org/alpine/edge/main
+mkdir -p /ccache && export CCACHE_DIR=${CCACHE_CHROOT_DIR} CCACHE_BASEDIR=/ PATH=/usr/lib/ccache/bin:\$PATH
+echo -e "${LIME}= Extracting source${NC}"
+tar xf ${SOCAT_TARBALL}
+cd socat-${SOCAT_VERSION}/
+echo -e "${PEACH}= Configure source${NC}"
+patch -p1 --fuzz=4 < ../hotfix-const-correctness.patch
+echo -e "${PEACH}= Configure source${NC}"
+./configure --enable-openssl --disable-ip6 --enable-readline --enable-largefile --enable-default-ipv=4 \
+  LDFLAGS='${BLDFLAGS} ${MOLD} -static-pie -w -Wl,-s' PKG_CONFIG='${PKGCFG}' \
+  CFLAGS='${BCFLAGS} ${ARCH_FLAGS} ${EXTRA} ${LTO} -fPIE'
+echo -e "${VIOLET}= Building...${NC}"
+make -j\$(nproc)
+echo -e "${CHARTREUSE}= Stripping binary${NC}"
+strip socat
+echo -e "${PURPLE_BLUE}= Compressing with UPX${NC}"
+upx --lzma socat
+EOF
+
+package_output "socat" "./${CHROOTDIR}/socat-${SOCAT_VERSION}/socat"
