0bc622eb565bd6b87cc436a7bed4c757a744850f
Matt Rinsch <gfunkmonk@mailbox.org>
2026-05-15 16:50:06 -0400

    un-fix

--- rsync-static-musl.sh
+++ rsync-static-musl.sh
@@ -19,9 +19,9 @@ run_build_setup "rsync" "${RSYNC_VERSION}" "${RSYNC_TARBALL}" \
 sudo chroot "./${CHROOTDIR}/" /bin/sh -s <<EOF
 set -e
 echo -e "${ORANGE}= Installing dependencies...${NC}"
-apk update && apk add build-base mold pkgconfig clang acl-dev acl-static attr-dev zstd-dev zstd-static openssl-libs-static openssl-dev git lz4-dev xxhash-dev
+apk update && apk add build-base mold ccache pkgconfig clang acl-dev acl-static attr-dev zstd-dev zstd-static openssl-libs-static openssl-dev git lz4-dev xxhash-dev
 apk upgrade musl-dev mold --repository=https://dl-cdn.alpinelinux.org/alpine/edge/main
-#mkdir -p /ccache && export CCACHE_DIR=${CCACHE_CHROOT_DIR} CCACHE_BASEDIR=/ PATH=/usr/lib/ccache/bin:\$PATH
+mkdir -p /ccache && export CCACHE_DIR=${CCACHE_CHROOT_DIR} CCACHE_BASEDIR=/ PATH=/usr/lib/ccache/bin:\$PATH
 echo -e "${CANARY}= Build & install xxHash${NC}"
 git clone https://github.com/Cyan4973/xxHash.git
 cd xxHash
